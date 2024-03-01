3b3772737ad223c0f3ed50891000b682b2a8cc34
ayoub <ayoubmassaadi2019@gmail.com>
2024-03-01 19:41:43 +0100

    v1

---
+++ src/main/java/com/example/springbootpatient/SpringbootPatientApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootpatient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootPatientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootPatientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootpatient/SpringbootPatientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootpatient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootPatientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
